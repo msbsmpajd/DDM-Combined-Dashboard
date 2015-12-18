84df2fdfee59b6249116d0b83bf8265c7325ec1e
msbsmpajd <stottele.mary@gmail.com>
2015-12-18 12:57:00 -0500

    Create ui.R

---
+++ ui.R
@@ -0,0 +1,40 @@
+library(shiny)
+library(ggmap)
+library(ggplot2)
+
+fluidPage(
+  #the name of title
+  titlePanel("Immigrant Eligibility"),
+  
+  #siebarpanel
+  sidebarPanel(
+    
+    #create variables "year“，we can add more here
+    selectInput('year', 'Years since Training', c("1","2","3","4","5","6","7","8","9")),
+    
+    selectInput('Documentation', 'Type of Documentation', c("I-551 CU","I-551 F","I-688B","I-766 a10","I-94","I-94 order of supervision","Prima Facie")),
+    
+    selectInput("center", "Center:", 
+                choices= c("Concourse", "Coney_Island", "Crotona",
+                           "East_NY", "Fort_Greene", "Jamaica",
+                           "Queens", "Refugee", "Richmond", "Rockaway",
+                           "St_Nicholas", "Washington_Heights", "Waverly",
+                           "Williamsburg"))
+  ),
+  
+  #mainpanel
+  mainPanel(
+    tabsetPanel(type = "tabs",
+                
+                tabPanel("Map",
+                         plotOutput('plot1', height="500")),
+                
+                tabPanel("Errors by Documents",
+                         plotOutput('ErrorPlot')),
+                
+                tabPanel("Errors by Staff",
+                         plotOutput('stafferrorPlot'))
+                
+    )
+  )
+)
